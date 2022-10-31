6598053a615233f90eb40c114e2e9d95e9784893
Даниил Шило <crckhd10@gmail.com>
2022-11-01 00:10:30 +0300

    feat(vscode): add commands

--- apps/frontend/src/App.vue
+++ apps/frontend/src/App.vue
@@ -5,18 +5,35 @@ import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
+  <div>
     <div>
-        <div>
-            <a href="https://vitejs.dev" target="_blank">
-                <img src="/vite.svg" class="logo" alt="Vite logo" />
-            </a>
-            <a href="https://vuejs.org/" target="_blank">
-                <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-            </a>
-        </div>
-
-        <HelloWorld msg="Vite + Vue" @click="() => {}" />
+      <a
+        href="https://vitejs.dev"
+        target="_blank"
+      >
+        <img
+          src="/vite.svg"
+          class="logo"
+          alt="Vite logo"
+        >
+      </a>
+      <a
+        href="https://vuejs.org/"
+        target="_blank"
+      >
+        <img
+          src="./assets/vue.svg"
+          class="logo vue"
+          alt="Vue logo"
+        >
+      </a>
     </div>
+
+    <HelloWorld
+      msg="Vite + Vue"
+      @click="() => { }"
+    />
+  </div>
 </template>
 
 <style scoped>
@@ -25,9 +42,11 @@ import HelloWorld from "./components/HelloWorld.vue";
     padding: 1.5em;
     will-change: filter;
 }
+
 .logo:hover {
     filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
     filter: drop-shadow(0 0 2em #42b883aa);
 }
